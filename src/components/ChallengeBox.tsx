2c6bd693e31d86448bfbfc4de74bc0dce3ac6ef1
priolilucas1 <priolilucas1@gmail.com>
2021-02-24 11:50:09 -0300

    Feature: Challenge-taking functionality created 🛹

--- src/components/ChallengeBox.tsx
+++ src/components/ChallengeBox.tsx
@@ -4,7 +4,7 @@ import { ChallengesContext } from "../contexts/ChallengesContext";
 import styles from "../styles/components/ChallengeBox.module.css";
 
 export default function ChallengeBox() {
-  const { activeChallenge } = useContext(ChallengesContext);
+  const { activeChallenge, resetChallenge } = useContext(ChallengesContext);
 
   return (
     <div className={styles.challengeBoxContainer}>
@@ -19,7 +19,11 @@ export default function ChallengeBox() {
           </main>
 
           <footer>
-            <button type="button" className={styles.challengeFailedButton}>
+            <button
+              type="button"
+              className={styles.challengeFailedButton}
+              onClick={resetChallenge}
+            >
               Falhei
             </button>
             <button type="button" className={styles.challengeSucceededButton}>
