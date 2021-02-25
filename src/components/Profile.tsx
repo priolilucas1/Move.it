b26e300f9e6e6ec16331ce7863d57c4238d05347
priolilucas1 <priolilucas1@gmail.com>
2021-02-25 09:41:52 -0300

    Context level imported

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,13 +1,18 @@
+import { useContext } from "react";
+import { ChallengesContext } from "../contexts/ChallengesContext";
 import styles from "../styles/components/Profile.module.css";
 
 export default function Profile() {
+  const { level } = useContext(ChallengesContext);
+
   return (
     <div className={styles.profileContainer}>
       <img src="https://github.com/priolilucas1.png" alt="Lucas Prioli" />
       <div>
         <strong>Lucas Prioli</strong>
         <p>
-          <img src="icons/level.svg" alt="Level" /> Level 2
+          <img src="icons/level.svg" alt="Level" />
+          Level {level}
         </p>
       </div>
     </div>
