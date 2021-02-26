c887b18601ad5abed2ed4123ffe42c1d4a30a42e
priolilucas1 <priolilucas1@gmail.com>
2021-02-26 12:39:48 -0300

    Feature: Level Up Modal created ⬆

---
+++ src/components/LevelUpModal.tsx
@@ -0,0 +1,25 @@
+import { useContext } from "react";
+import { ChallengesContext } from "../contexts/ChallengesContext";
+import styles from "../styles/components/LevelUpModal.module.css";
+
+export default function LevelUpModal() {
+  const { level, closeLevelUpModal } = useContext(ChallengesContext);
+  return (
+    <div className={styles.overlay}>
+      <div className={styles.container}>
+        <header>{level}</header>
+
+        <strong>Parabéns</strong>
+        <p>Você alcançou um novo level.</p>
+
+        <button
+          type="button"
+          onClick={closeLevelUpModal}
+          style={{ outline: "none" }}
+        >
+          <img src="/icons/close.svg" alt="Close Modal" />
+        </button>
+      </div>
+    </div>
+  );
+}
