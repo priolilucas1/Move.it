141b493291e67b2aaaa1ed11dc9911830c4a5772
priolilucas1 <priolilucas1@gmail.com>
2021-03-01 17:55:51 -0300

    Feature: Login screen done

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,62 +1,76 @@
 import Head from "next/head";
-import { GetServerSideProps } from "next";
 
-import ExperienceBar from "../components/ExperienceBar";
-import Profile from "../components/Profile";
-import CompletedChallenges from "../components/CompletedChallenges";
-import Countdown from "../components/Countdown";
-import ChallengeBox from "../components/ChallengeBox";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import { FaGithub, FaArrowRight } from "react-icons/fa";
 
-import { CountdownProvider } from "../contexts/CoutdownContext";
-import { ChallengesProvider } from "../contexts/ChallengesContext";
+import styles from "../styles/Pages/Login.module.css";
 
-import styles from "../styles/Pages/Home.module.css";
+export default function Login() {
+  const router = useRouter();
+  const [username, setUsername] = useState("");
+  const [toogleColorBtt, setToogleColorBtt] = useState(false);
 
-interface HomeProps {
-  level: number;
-  currentXp: number;
-  challengesCompleted: number;
-}
+  function submit(e: any) {
+    e.preventDefault();
+    if (username) {
+      router.push(`/${username}`);
+    }
+  }
 
-export default function Home(props: HomeProps) {
+  useEffect(() => {
+    if (username.length >= 4) {
+      setToogleColorBtt(true);
+    }
+
+    if (username.length < 4) {
+      setToogleColorBtt(false);
+    }
+  }, [username]);
+
   return (
-    <ChallengesProvider
-      level={props.level}
-      currentXp={props.currentXp}
-      challengesCompleted={props.challengesCompleted}
-    >
-      <div className={styles.container}>
-        <Head>
-          <title>Início | Move.it</title>
-        </Head>
+    <div className={styles.loginContainer}>
+      <Head>
+        <title>Home | Move.it</title>
+      </Head>
 
-        <ExperienceBar />
+      <div className={styles.content}>
+        <img src="logoLogin.png" alt="Logo" />
 
-        <CountdownProvider>
-          <section>
-            <div>
-              <Profile />
-              <CompletedChallenges />
-              <Countdown />
+        <div className={styles.gitHubContainer}>
+          <div>
+            <strong>Bem-vindo</strong>
+          </div>
+          <div className={styles.gitHubIconText}>
+            <FaGithub size={40} />
+            <div className={styles.text}>
+              Faça login com seu Github <br />
+              para começar
             </div>
-            <div>
-              <ChallengeBox />
+          </div>
+          <div className={styles.inputLogin}>
+            <div className={styles.input}>
+              <input
+                type="text"
+                placeholder="Digite seu username"
+                onChange={(e) => setUsername(e.target.value)}
+              />
             </div>
-          </section>
-        </CountdownProvider>
+            <button
+              type="submit"
+              className={styles.button}
+              onClick={submit}
+              style={
+                toogleColorBtt
+                  ? { background: "#4cd62b" }
+                  : { background: "#4953b8" }
+              }
+            >
+              <FaArrowRight size={24} />
+            </button>
+          </div>
+        </div>
       </div>
-    </ChallengesProvider>
+    </div>
   );
 }
-
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  const { level, currentXp, challengesCompleted } = context.req.cookies;
-
-  return {
-    props: {
-      level: Number(level),
-      currentXp: Number(currentXp),
-      challengesCompleted: Number(challengesCompleted),
-    },
-  };
-};
